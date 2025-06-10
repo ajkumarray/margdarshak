36820368a8ba4b080f25abfed7ee57e78bea9eec
ajkumarray <ajit.kumar@docquity.com>
2025-06-10 18:37:09 +0530

    chore:trigger re-run

--- src/main/java/com/ajkumarray/margdarshak/config/SecurityConfig.java
+++ src/main/java/com/ajkumarray/margdarshak/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
+    } 
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
