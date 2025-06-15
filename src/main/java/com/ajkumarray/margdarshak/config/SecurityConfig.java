4233c3d6f149d2a051a3ff41b42198dbeb790ca5
ajkumarray <ajit.kumar@docquity.com>
2025-06-15 13:57:24 +0530

    feat:disabled csrf again as was getting 403 on public apis

--- src/main/java/com/ajkumarray/margdarshak/config/SecurityConfig.java
+++ src/main/java/com/ajkumarray/margdarshak/config/SecurityConfig.java
@@ -24,9 +24,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.cors(cors -> cors.configurationSource(corsConfigurationSource())).authorizeHttpRequests(auth -> auth
-                .requestMatchers("/*", "/api/v1/auth/**", "api/v1/public/**", "/swagger-ui/**", "/v3/api-docs/**")
-                .permitAll().anyRequest().authenticated())
+        return http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/*", "/api/v1/auth/**", "api/v1/public/**",
+                        "/swagger-ui/**", "/v3/api-docs/**").permitAll().anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
@@ -41,7 +41,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    } 
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
